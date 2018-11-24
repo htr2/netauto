dff4c25d617b0841f8158da34d402b620b87e848
unknown <hvoelksen@gmail.com>
2018-11-24 16:13:30 +0000

    added restapi playbook

--- mgmt.sh
+++ mgmt.sh
@@ -2,6 +2,11 @@
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo yum -y install code
+code --install-extension marcostazi.VS-code-vagrantfile
+code --install-extension michelemelluso.gitignore
+code --install-extension ms-python.python
+code --install-extension ms-vscode.notepadplusplus-keybindings
+code --install-extension redhat.vscode-yaml
 sudo yum -y groupinstall development
 sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
